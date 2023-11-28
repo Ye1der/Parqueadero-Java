04fb3ff45c553b8fcf29e17c5dca1ee0d30dad58
Yeider <yeiderjp12@gmail.com>
2023-11-27 23:15:19 -0500

    Vista Login y Menu principal hechas

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -14,21 +14,8 @@ import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
-      ClienteCtrl cliente = new ClienteCtrl();
-
-      for (ClienteTO clienteTO : cliente.listar()) {
-          System.out.println("id: " + clienteTO.getIdCliente());
-          System.out.println("nombre: " + clienteTO.getNombre());
-          System.out.println("cedula: " + clienteTO.getCedula());
-          System.out.println("mensualidad: " + clienteTO.getMensualidad() + "\n");
-      }
-
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-      LocalDateTime fechaAhora = LocalDateTime.parse(LocalDateTime.now().format(formatter), formatter);
-      System.out.println(LocalDateTime.now().format(formatter));
-      System.out.println(fechaAhora);
-
       FacturaDAO factura = new FacturaDAO();
-      factura.crearHoraEntrada();
+      // factura.crearHoraEntrada("XYZ-123");
+      factura.generarFactura("XYZ-123");
   }
 }
